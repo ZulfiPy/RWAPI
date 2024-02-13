276aec74deda286383a80924f9b2c851472a36b3
ZulfiPy <zulfizupy@gmail.com>
2024-02-13 08:29:46 +0200

    verifyRoles middleware connected to the CRUD routes

--- routes/api/customers.js
+++ routes/api/customers.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const verifyRoles = require('../../middleware/verifyRoles');
+const ROLES_LIST = require('../../config/roles_list');
 
 const customersController = require('../../controllers/customersController');
 
 router.route('/')
-    .get(customersController.getCustomers)
-    .post(customersController.createCustomer)
-    .put(customersController.updateCustomer)
-    .delete(customersController.deleteCustomer)
+    .get(verifyRoles([ROLES_LIST.User, ROLES_LIST.Editor, ROLES_LIST.Admin]),customersController.getCustomers)
+    .post(verifyRoles([ROLES_LIST.Editor, ROLES_LIST.Admin]), customersController.createCustomer)
+    .put(verifyRoles([ROLES_LIST.Editor, ROLES_LIST.Admin]), customersController.updateCustomer)
+    .delete(verifyRoles([ROLES_LIST.Admin]), customersController.deleteCustomer)
 
 router.route('/:id')
-    .get(customersController.getCustomer)
-    .put(customersController.updateCustomer)
-    .delete(customersController.deleteCustomer)
+    .get(verifyRoles([ROLES_LIST.User, ROLES_LIST.Editor, ROLES_LIST.Admin]), customersController.getCustomer)
+    .put(verifyRoles([ROLES_LIST.Editor, ROLES_LIST.Admin]), customersController.updateCustomer)
+    .delete(verifyRoles([ROLES_LIST.Admin]), customersController.deleteCustomer)
 
 module.exports = router;
