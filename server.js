7089d2a644b222919d313e4e538d3e603dd5ab7c
ZulfiPy <zulfizupy@gmail.com>
2024-02-13 07:04:33 +0200

    use verify JWT middleware; comments added

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT;
 const mongoose = require('mongoose');
 const connectToDB = require('./config/dbConnect');
 const cookiesParser = require('cookie-parser')
+const verifyJWT = require('./middleware/verifyJWT');
 
 // run database connection config
 connectToDB();
@@ -13,17 +14,23 @@ app.get('/', (req, res) => {
     res.send('hello world');
 });
 
+// built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
+// built-in middleware for json
 app.use(express.json());
 
+// middleware for parsing cookies
 app.use(cookiesParser());
 
+// routes
 app.use('/register', require('./routes/registerEmployee'));
 app.use('/auth', require('./routes/auth'));
 app.use('/refresh', require('./routes/refresh'));
 app.use('/logout', require('./routes/logout'));
 
+// verify JWT before making request to the API
+app.use(verifyJWT)
 app.use('/api/employees', require('./routes/api/employees'));
 app.use('/api/customers', require('./routes/api/customers'));
 
